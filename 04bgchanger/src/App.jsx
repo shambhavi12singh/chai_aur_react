8c24e21674241072d4a720b81f3a2dbaef87573b
shambhavi12singh <143245947+shambhavi12singh@users.noreply.github.com>
2024-02-16 21:08:57 +0530

    bgchanger

---
+++ 04bgchanger/src/App.jsx
@@ -0,0 +1,35 @@
+import { useState } from 'react'
+
+
+function App() {
+  //const [count, setCount] = useState(0)
+  const [color, setColor]=useState("olive")
+  return (
+    <div className='w-full h-screen duration-200' style = {{backgroundColor : color}}>
+      <div
+      className='fixed flex flex-wrap justify-center bottom-12 inset-x-0 px-2'>
+        <div className='flex flex-wrap justify-center gap-3 shadow-2xl bg-white rounded-xl px-2 py-3'> 
+        <button onClick={() => setColor("red")}
+        className='outline-none px-4  py-1 rounded-full text-white shadow-lg' style={{backgroundColor:"red"}}>Red</button>
+        <button onClick={() => setColor("yellow")}
+        className='outline-none px-4  py-1 rounded-full text-white shadow-lg' style={{backgroundColor:"yellow"}}>Yellow</button>
+        <button onClick={() => setColor("green")}
+        className='outline-none px-4  py-1 rounded-full text-white shadow-lg' style={{backgroundColor:"green"}}>Green</button>
+        <button onClick={() => setColor("blue")}
+        className='outline-none px-4  py-1 rounded-full text-white shadow-lg' style={{backgroundColor:"blue"}}>Blue</button>
+        <button onClick={() => setColor("orange")}
+        className='outline-none px-4  py-1 rounded-full text-white shadow-lg' style={{backgroundColor:"orange"}}>Orange</button>
+        <button onClick={() => setColor("magenta")}
+        className='outline-none px-4  py-1 rounded-full text-white shadow-lg' style={{backgroundColor:"magenta"}}>Magenta</button>
+        <button onClick={() => setColor("brown")}
+        className='outline-none px-4  py-1 rounded-full text-white shadow-lg' style={{backgroundColor:"brown"}}>Brown</button>
+        <button onClick={() => setColor("lightgreen")}
+        className='outline-none px-4  py-1 rounded-full text-white shadow-lg' style={{backgroundColor:"lightgreen"}}>Lightgreen</button>
+         </div>
+      </div>
+
+    </div>
+  )
+}
+
+export default App
